b717d30c01f21406f184eb5021878d4f32f2ddd0
Девятых Мария Алексеевна <m.dev@skbkontur.ru>
2019-10-12 16:37:33 +0500

    fix

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -15,40 +15,39 @@ Buzzer buzzer1(PIN_BUZZER1);
 Buzzer buzzer2(PIN_BUZZER2);
 Buzzer buzzer3(PIN_BUZZER3);
 Buzzer buzzer4(PIN_BUZZER4);
+Buzzer buzzers[] = {buzzer1, buzzer2, buzzer3, buzzer4};
 
 bool start = false;
-int notes[] = {NOTE_G3};
+int notes1[] = {NOTE_G3};
+int notes2[] = {NOTE_A2};
 double durations[] = {8};
 int melodyLength = 1;
+int currentBuzzer = 0;
+int soundDuration = 1000;
+uint64_t lastMillis;
 
 void setup() {
-    Serial.begin(115200);
-    buzzer1.setMelody(notes, durations, melodyLength);
-    buzzer1.turnSoundOn();
-    buzzer2.setMelody(notes, durations, melodyLength);
-    buzzer2.turnSoundOn();
-    buzzer3.setMelody(notes, durations, melodyLength);
-    buzzer3.turnSoundOn();
-    buzzer4.setMelody(notes, durations, melodyLength);
-    buzzer4.turnSoundOn();
-
+    buzzers[0].setMelody(notes1, durations, melodyLength);
+    buzzers[1].setMelody(notes2, durations, melodyLength);
+    buzzers[2].setMelody(notes1, durations, melodyLength);
+    buzzers[3].setMelody(notes2, durations, melodyLength);
 }
 
 void loop() {
 
     if (buttonOn.wasPressed()){
       start = true;
+      buzzers[currentBuzzer].turnSoundOn();
+      lastMillis = millis();
     }
 
     if (start){
-
-      Buzzer buzzers[] = {buzzer1, buzzer2, buzzer3, buzzer4};
-  
-      for(int i = 0; i < 5; i++){
-        Serial.write(i);
-        buzzers[i % 4].playSound();
-        delay(1000);
-        buzzers[i % 4].turnSoundOff();
+      buzzers[currentBuzzer % 4].playSound();
+      if ((millis() - lastMillis) > soundDuration){
+        buzzers[currentBuzzer % 4].turnSoundOff();
+        currentBuzzer++;
+        buzzers[currentBuzzer % 4].turnSoundOn();
+        lastMillis = millis();
       }
     }
     
