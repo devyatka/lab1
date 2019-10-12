5d20299c5e6fac84326ee8f2098141e52e382e12
Девятых Мария Алексеевна <m.dev@skbkontur.ru>
2019-10-12 15:36:14 +0500

    sirena

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -3,26 +3,53 @@
 #include "button.h"
 #include "buzzer.h"
 
-#define PIN_BUZZER 6
-#define PIN_BUTTON_OFF 5
+#define PIN_BUZZER1 5
+#define PIN_BUZZER2 2
+#define PIN_BUZZER3 3
+#define PIN_BUZZER4 4
+#define PIN_BUTTON_ON 6
 
-Button buttonOff(PIN_BUTTON_OFF);
-Buzzer buzzer(PIN_BUZZER);
 
+Button buttonOn(PIN_BUTTON_ON); 
+Buzzer buzzer1(PIN_BUZZER1);
+Buzzer buzzer2(PIN_BUZZER2);
+Buzzer buzzer3(PIN_BUZZER3);
+Buzzer buzzer4(PIN_BUZZER4);
+
+bool start = false;
 int notes[] = {NOTE_G3};
 double durations[] = {8};
 int melodyLength = 1;
 
 void setup() {
-    buzzer.setMelody(notes, durations, melodyLength);
-    buzzer.turnSoundOn();
+    Serial.begin(115200);
+    buzzer1.setMelody(notes, durations, melodyLength);
+    buzzer1.turnSoundOn();
+    buzzer2.setMelody(notes, durations, melodyLength);
+    buzzer2.turnSoundOn();
+    buzzer3.setMelody(notes, durations, melodyLength);
+    buzzer3.turnSoundOn();
+    buzzer4.setMelody(notes, durations, melodyLength);
+    buzzer4.turnSoundOn();
+
 }
 
 void loop() {
-  
-    buzzer.playSound();
-    if (buttonOff.wasPressed())
-    {
-        buzzer.turnSoundOff();
+
+    if (buttonOn.wasPressed()){
+      start = true;
     }
+
+    if (start){
+
+      Buzzer buzzers[] = {buzzer1, buzzer2, buzzer3, buzzer4};
+  
+      for(int i = 0; i < 5; i++){
+        Serial.write(i);
+        buzzers[i % 4].playSound();
+        delay(1000);
+        buzzers[i % 4].turnSoundOff();
+      }
+    }
+    
 }
